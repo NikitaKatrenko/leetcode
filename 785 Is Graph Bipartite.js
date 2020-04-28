bfa43d61c06c728c6b58cb25e07b341a351d9771
Mykyta Popov <mykytapopov@TX-N-0292.local>
2020-04-28 21:30:02 +0300

    add queue class

--- 785 Is Graph Bipartite.js
+++ 785 Is Graph Bipartite.js
@@ -3,7 +3,44 @@
  * @return {boolean}
  */
 var isBipartite = function (graph) {
+    class Queue
+    {
+        // Array is used to implement a Queue
+        constructor() {
+            this.items = [];
+        }
 
+        // Functions to be implemented
+        enqueue(element) {
+            // adding element to the queue
+            this.items.push(element);
+        }
+        dequeue() {
+            // removing element from the queue
+            // returns underflow when called
+            // on empty queue
+            if(this.isEmpty())
+                return "Underflow";
+            return this.items.shift();
+        }
+        front() {
+            // returns the Front element of
+            // the queue without removing it.
+            if(this.isEmpty())
+                return "No elements in Queue";
+            return this.items[0];
+        }
+        isEmpty() {
+            // return true if the queue is empty.
+            return this.items.length == 0;
+        }
+        printQueue() {
+            var str = "";
+            for(var i = 0; i < this.items.length; i++)
+                str += this.items[i] +" ";
+            return str;
+        }
+    }
     // create a graph class
     class Graph {
         // defining vertex array and
@@ -52,8 +89,95 @@ var isBipartite = function (graph) {
             }
         }
 
-        // bfs(v)
-        // dfs(v)
+        // function to performs BFS
+        bfs(startingNode) {
+            // create a visited array
+            var visited = [];
+            for (var i = 0; i < this.noOfVertices; i++)
+                visited[i] = false;
+
+            // Create an object for queue
+            var q = new Queue();
+
+            // add the starting node to the queue
+            visited[startingNode] = true;
+            q.enqueue(startingNode);
+
+            // loop until queue is element
+            while (!q.isEmpty()) {
+                // get the element from the queue
+                var getQueueElement = q.dequeue();
+
+                // passing the current vertex to callback funtion
+                console.log(getQueueElement);
+
+                // get the adjacent list for current vertex
+                var get_List = this.AdjList.get(getQueueElement);
+
+                // loop through the list and add the element to the
+                // queue if it is not processed yet
+                for (var i in get_List) {
+                    var neigh = get_List[i];
+
+                    if (!visited[neigh]) {
+                        visited[neigh] = true;
+                        q.enqueue(neigh);
+                    }
+                }
+            }
+        }
+
+        // function to preform DFS
+        dfs(startingNode) {
+            var visited = [];
+            for (var i = 0; i < this.noOfVertices; i++)
+                visited[i] = false;
+
+            this.DFSUtil(startingNode, visited);
+        }
+
+        // Recursive function which process and explore
+        // all the adjacent vertex of the vertex with which it is called
+        DFSUtil(vert, visited) {
+            visited[vert] = true;
+            console.log(vert);
+
+            var get_neighbours = this.AdjList.get(vert);
+
+            for (var i in get_neighbours) {
+                var get_elem = get_neighbours[i];
+                if (!visited[get_elem])
+                    this.DFSUtil(get_elem, visited);
+            }
+        }
     }
 
+    var g = new Graph(6);
+    var vertices = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+    // adding vertices
+    for (var i = 0; i < vertices.length; i++) {
+        g.addVertex(vertices[i]);
+    }
+
+    // adding edges
+    g.addEdge('A', 'B');
+    g.addEdge('A', 'D');
+    g.addEdge('A', 'E');
+    g.addEdge('B', 'C');
+    g.addEdge('D', 'E');
+    g.addEdge('E', 'F');
+    g.addEdge('E', 'C');
+    g.addEdge('C', 'F');
+
+    // prints all vertex and
+    // its adjacency list
+    // A -> B D E
+    // B -> A C
+    // C -> B E F
+    // D -> A E
+    // E -> A D F C
+    // F -> E C
+    // g.printGraph();
+    g.bfs('A');
 };
