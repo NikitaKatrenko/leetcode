d01552a23e069f4f9c58acad08e79c3eb65a634a
Mykyta Popov <mykytapopov@TX-N-0292.local>
2020-05-07 15:15:49 +0300

    3. Longest Substring Without Repeating Characters

---
+++ 3 Longest Substring Without Repeating Characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let tmp = '';
+    let res = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let index = tmp.indexOf(s[i]);
+
+        if (index !== -1) {
+            tmp = tmp.substring(index + 1);
+        }
+
+        tmp += s[i];
+        res = Math.max(res, tmp.length);
+    }
+
+
+    return res;
+};
